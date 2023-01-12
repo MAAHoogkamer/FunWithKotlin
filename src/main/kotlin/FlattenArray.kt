942c46ed30d1c17f7182e4fb0391e6bad54cc3dd
Michiel A. A. Hoogkamer <michiel.hoogkamer@staffbase.com>
2023-01-12 18:25:44 +0100

    Doesnt work

--- src/main/kotlin/FlattenArray.kt
+++ src/main/kotlin/FlattenArray.kt
@@ -1,4 +1,12 @@
+import foo.test.ModArray
+
 fun main() {
-        // var array = [1,[2,3,null,4],[null],5] in Kotlin looks like:
+        // var exampleArray = [1,[2,3,null,4],[null],5] in Kotlin looks like:
         val exampleArray: Array<Array<out Int?>> = arrayOf(arrayOf(1), arrayOf(2,3,null,4), arrayOf(null))
+
+        val modArray = ModArray() // Create instance modArray of ModArray
+        val flattenedArray = modArray.flattenArray(exampleArray).filterNotNull()
+        // The new val uses the class method
+        println(flattenedArray)
+
 }
--- src/main/kotlin/foo/test/ModArray.kt
+++ src/main/kotlin/foo/test/ModArray.kt
@@ -1,9 +1,12 @@
 package foo.test
 
 class ModArray {
-
-    fun flattenAray(input: Array<*>): Array<*> { // Kotlin needs type declaration, * accepts any datatype
-        val flattened = input.flatten() // Kotlin has this function for flattening
-        return flattened
+    // Doesn't work, try casting to list first
+    fun flattenArray(input: Array<*>): List<Any?> { // Kotlin needs type declaration, * is for unknown input type
+        // Return type changed from Array to List<Any>, while flatten() returns List
+        // Any is the supertype of all types in Kotlin, it can hold Any datatype
+        return input.flatMap { it as? Iterable<*> }
+        // flatten() only works with Collections like List.
     }
+
 }
