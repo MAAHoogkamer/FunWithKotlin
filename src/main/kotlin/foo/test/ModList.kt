f1b5cd8674f5efd6a072a98bf7a48fa20a2b1327
Michiel A. A. Hoogkamer <michiel.hoogkamer@staffbase.com>
2023-01-16 08:26:39 +0100

    Added ModList Test

--- src/main/kotlin/foo/test/ModList.kt
+++ src/main/kotlin/foo/test/ModList.kt
@@ -9,8 +9,11 @@ class ModList {
     }
     /*
     The flatMap function iterates through the list and applies a lambda function to each element.
-    The lambda function checks if the current element is an Iterable. If so,
-    it filters the null values and flattens it, otherwise it wraps the element in a list.
-    It then concatenates the results to produce a flattened list.
+    The lambda function checks if the current element is an Iterable*. If so,
+    it filters the null values and flattens it, otherwise it wraps the element in
+
+    * Iterable is an object which can be looped over or iterated over with the help of a for loop.
+    Objects like lists, tuples, sets, dictionaries, strings, etc. are called iterables.
+    In short and simpler terms, iterable is anything that you can loop over.
      */
 }
---
+++ src/test/kotlin/foo/test/ModListTest.kt
@@ -0,0 +1,10 @@
+package foo.test
+
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotEquals
+
+internal class ModListTest {
+
+
+}
